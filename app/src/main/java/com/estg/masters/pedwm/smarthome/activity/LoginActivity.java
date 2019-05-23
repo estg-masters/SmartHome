07cf10deb733abc9aec601327864598c11b1dcda
RicardooCorreia <ricardocorreiap@gmail.com>
2019-05-23 23:27:13 +0100

    Integrated Firebase Auth

---
+++ app/src/main/java/com/estg/masters/pedwm/smarthome/activity/LoginActivity.java
@@ -0,0 +1,98 @@
+package com.estg.masters.pedwm.smarthome.activity;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.estg.masters.pedwm.smarthome.R;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        GoogleSignInOptions googleSignInOptions =
+                new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+                .build();
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signInButton = findViewById(R.id.sign_in_button);
+
+        signInButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        // Todo: add user if don't exist
+        if (result.isSuccess()) {
+            firebaseAuthWithGoogle(result.getSignInAccount());
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()) {
+                        goToActivity(MainActivity.class);
+                    } else {
+                        Log.d("Error", "Auth failed!");
+                    }
+                });
+    }
+
+    public void goToActivity(Class activityToGo) {
+        Intent intent = new Intent(LoginActivity.this, activityToGo);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/estg/masters/pedwm/smarthome/activity/MainActivity.java
+++ app/src/main/java/com/estg/masters/pedwm/smarthome/activity/MainActivity.java
@@ -6,17 +6,38 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.estg.masters.pedwm.smarthome.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser == null) {
+            goToActivity(LoginActivity.class);
+        }
     }
 
     public void goToHousesView(View view) {
-        Intent intent = new Intent(MainActivity.this, HousesActivity.class);
+        goToActivity(HousesActivity.class);
+    }
+
+    public void goToActivity(Class activityToGo) {
+        Intent intent = new Intent(MainActivity.this, activityToGo);
         startActivity(intent);
     }
 }
