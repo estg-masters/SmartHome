114ee92c3ef414e929c2c91ed5e0c4fb6173f913
Pedro <pfmluis@gmail.com>
2019-06-17 00:41:11 +0100

    Added event handlers for the notifications

--- app/src/main/java/com/estg/masters/pedwm/smarthome/services/MyFirebaseMessagingService.java
+++ app/src/main/java/com/estg/masters/pedwm/smarthome/services/MyFirebaseMessagingService.java
@@ -1,7 +1,5 @@
 package com.estg.masters.pedwm.smarthome.services;
 
-import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -12,26 +10,11 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        super.onMessageReceived(remoteMessage);
-
-
-        Log.d("", "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            //TODO handle received message here
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            //TODO handle received notification here
-            Log.d("", "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
+        //TODO HANDLE NOTIFICATION HERE
     }
 
     @Override
     public void onNewToken(String s) {
-        super.onNewToken(s);
+        //TODO HANDLE TOKEN CHANGES HERE
     }
 }
